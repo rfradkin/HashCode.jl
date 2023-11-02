36e9dee34f70272ea8e10fd078e63be7010a3468
Documenter.jl <documenter@juliadocs.github.io>
2023-11-02 19:25:55 +0000

    build based on fa7fb8f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = hw6","category":"page"},{"location":"#hw6","page":"Home","title":"hw6","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for hw6.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [hw6]","category":"page"}]
+}
