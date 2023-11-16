7024c64995f1183a2c27dac1959b1ce8fc10cc22
Rom Fradkin <fradkin.rom@gmail.com>
2023-11-16 17:42:28 -0500

    try once more

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,23 @@
 # using CustomType
 using Test
 using HashCode2014
-using CustomType
+include("../src/CustomType.jl")
 
-@testset "CustomType.jl" begin
+@testset "jl" begin
     # Write your tests here.
     city = HashCode2014.read_city()
-    mapObject = CustomType.CityMap(city.junctions,city.streets)
-    res = CustomType.getCarLocation(mapObject,1)
+    mapObject = CityMap(city.junctions,city.streets)
+    res = getCarLocation(mapObject,1)
     @test res === 4517
-    CustomType.addCarMove(mapObject,1,15)
-    res2 = CustomType.getCarLocation(mapObject,1)
+    addCarMove(mapObject,1,15)
+    res2 = getCarLocation(mapObject,1)
     @test res2 === 15
     ans3 =  [(8261, 1),(9775, 1)]
-    @test CustomType.getPosJuncs(mapObject,4715)[1][1] === 8261
-    @test CustomType.getPosJuncs(mapObject,4715)[1][2] === 1
-    @test CustomType.getPosJuncs(mapObject,4715)[2][1] === 9775
-    @test CustomType.getPosJuncs(mapObject,4715)[2][2] === 1
+    @test getPosJuncs(mapObject,4715)[1][1] === 8261
+    @test getPosJuncs(mapObject,4715)[1][2] === 1
+    @test getPosJuncs(mapObject,4715)[2][1] === 9775
+    @test getPosJuncs(mapObject,4715)[2][2] === 1
 
-    @test CustomType.alreadyVisited(mapObject,15) 
+    @test alreadyVisited(mapObject,15) 
 
 end
