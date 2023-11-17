bad7167e9849d27ace26118c8b589a81373436d9
Rom Fradkin <fradkin.rom@gmail.com>
2023-11-16 21:01:03 -0500

    new technique

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,10 @@
-using HashCode
+# using HashCode
 using Documenter
 
 DocMeta.setdocmeta!(HashCode, :DocTestSetup, :(using HashCode); recursive=true)
 
 makedocs(;
-    modules=[HashCode],
+    modules=["src/HashCode.jl", "src/CustomTypes.jl"],
     authors="Rom Fradkin <fradkin.rom@gmail.com> and contributors",
     repo="https://github.com/rfradkin/HashCode.jl/blob/{commit}{path}#{line}",
     sitename="HashCode.jl",
