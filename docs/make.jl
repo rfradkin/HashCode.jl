e10cb87475e2141a40313fda8cb0efd3ff8d9d31
Rom Fradkin <fradkin.rom@gmail.com>
2023-11-16 19:20:58 -0500

    typo

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,7 @@ using Documenter
 DocMeta.setdocmeta!(HashCode, :DocTestSetup, :(using HashCode); recursive=true)
 
 makedocs(;
-    modules=[HashCode, CustomType],
+    modules=[HashCode, CustomTypes],
     authors="Rom Fradkin <fradkin.rom@gmail.com> and contributors",
     repo="https://github.com/rfradkin/HashCode.jl/blob/{commit}{path}#{line}",
     sitename="HashCode.jl",
